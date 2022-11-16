1a1aa527140c134729bc0037fa23128e95275264
Roy <Roy@LAPTOPROY>
2022-11-16 16:21:08 +0100

    Model user

---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Resallie.Models
+{
+    public class User : Model
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryId
+        {
+            get => Id;
+            set => Id = value;
+        }
+
+        [Required] public string Email { get; set; }
+        [Required] public string Password { get; set; }
+        [Required] public string FirstName { get; set; }
+        [Required] public string LastName { get; set; }
+        [Required] public string Gender { get; set; }
+        [Required] public string Phonenumber { get; set; }
+        [Required] public DateTime EmailVeriviedAt { get; set; }
+    }
+}
