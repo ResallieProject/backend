3a6247cf383213ba02c00d678b24c2e33808cb68
Michel3951 <tfhdgnetwork1@gmail.com>
2022-12-01 19:08:05 +0100

    add user model

---
+++ Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Resallie.Models;
+
+public class User : Model
+{
+    [StringLength(128)]
+    [Required]
+    public string Email { get; set; }
+    
+    [JsonIgnore]
+    [StringLength(512)]
+    [Required]
+    public string Password { get; set; }
+    
+    [StringLength(32)]
+    [Required]
+    public string FirstName { get; set; }
+    
+    [StringLength(64)]
+    [Required]
+    public string LastName { get; set; }
+    
+    [StringLength(32)]
+    public string? Gender { get; set; }
+    
+    [Required]
+    [StringLength(32)]
+    public string Phone { get; set; }
+
+    [Timestamp]
+    public DateTime? EmailVerifiedAt { get; set; }
+}
