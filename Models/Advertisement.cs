f0b815f073288c0a87068c7f69e0bd8aa2aed1b5
Roy <Roy@LAPTOPROY>
2022-12-07 10:55:09 +0100

    start of model en migration

--- Models/Advertisement.cs
+++ Models/Advertisement.cs
@@ -1,9 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 using Resallie.Data;
 using Bogus;
-using Microsoft.EntityFrameworkCore;
 
 namespace Resallie.Models
 {
---
+++ Models/AdvertisementImages.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Resallie.Models
+{
+    public class AdvertisementImages : Model
+    {
+        [Required]
+        public string Path { get; set; }
+        public uint Order { get; set; }
+
+        [ForeignKey("Advertisement")]
+        [Required]
+        public int AdvertisementId { get; set; }
+    }
+}
