815412cc3cbfdc14fc0f86d059ada444a1bbe601
Roy <Roy@LAPTOPROY>
2022-12-08 13:35:12 +0100

    adding interfaces

---
+++ Resallie.Tests/Advertisement/Interfaces/FakeDbSet.cs
@@ -0,0 +1,64 @@
+﻿using Resallie.Data.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resallie.Tests.Advertisement.Interfaces
+{
+    public class FakeDbSet<T> : IDbSet<T> where T : class
+    {
+        public ObservableCollection<T> Local => throw new NotImplementedException();
+
+        public Type ElementType => throw new NotImplementedException();
+
+        public Expression Expression => throw new NotImplementedException();
+
+        public IQueryProvider Provider => throw new NotImplementedException();
+
+        public T Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Attach(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Find(params object[] keyValues)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        TDerivedEntity IDbSet<T>.Create<TDerivedEntity>()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
