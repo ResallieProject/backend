d3ebea0e33577a11a3edd63e54589595b728b57c
Roy <Roy@LAPTOPROY>
2023-01-18 13:17:10 +0100

    Finaly working

--- Respositories/Advertisements/AdvertisementImagesRepository.cs
+++ Respositories/Advertisements/AdvertisementImagesRepository.cs
@@ -1,41 +1,44 @@
-﻿    using Minio;
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Minio;
+using Minio.DataModel;
 using Minio.Exceptions;
 using Resallie.Data;
-    using Resallie.Models.Advertisements;
+using Resallie.Models.Advertisements;
 
-    namespace Resallie.Respositories.Advertisements
+namespace Resallie.Respositories.Advertisements
+{
+    public class AdvertisementImagesRepository
     {
-        public class AdvertisementImagesRepository
-        {
-            private static IWebHostEnvironment _environment;
-            private AppDbContext _ctx;
+        //private static IWebHostEnvironment _environment;
+        private readonly AppDbContext _ctx;
 
         public AdvertisementImagesRepository(AppDbContext ctx, IWebHostEnvironment environment)
         {
             _ctx = ctx;
-            _environment = environment;
+            //_environment = environment;
         }
 
-        //[Obsolete]
-        public async Task StoreImages(Advertisement advertisement, IFormFileCollection objFiles)
+        [Obsolete]
+        public async void StoreImages(Advertisement advertisement, IFormFileCollection objFiles)
         {
             //Acces Key ID: SCWPMTJRRN78RY602TM4 
             //Secret Key: 0085c57c-6b76-4ea6-849d-510ea7b9125e
             //Project ID: 5be1c470-325a-40da-8187-cdcbbe62b418
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11
+                                                 | SecurityProtocolType.Tls12;
             try
             {
-                var minio = new MinioClient(
-                    "s3.nl-ams.scw.cloud",
-                    "SCWPMTJRRN78RY602TM4",
-                    "0085c57c-6b76-4ea6-849d-510ea7b9125e",
-                    "nl-ams"
-                    ).WithSSL();
+                var client = new MinioClient()
+                    .WithEndpoint("s3.nl-ams.scw.cloud")
+                    .WithRegion("nl-ams")
+                    .WithCredentials("SCWPMTJRRN78RY602TM4",
+                                     "0085c57c-6b76-4ea6-849d-510ea7b9125e")
+                    .WithSSL()
+                    .Build();
 
-                string[] fileEntries = Directory.GetFiles("/app");
-                foreach (string fileName in fileEntries)
-                {
-                    Run(minio, fileName).Wait();
-                }
+                Run(client, objFiles);
             }
             catch (Exception ex)
             {
@@ -43,17 +46,41 @@ using Resallie.Data;
             }
         }
 
-        private async static Task Run(MinioClient minio, string file)
+        public async Task Run(MinioClient client, IFormFileCollection objFiles)
         {
-            try
-            {
-                await minio.PutObjectAsync("my-bucket", Path.GetFileName(file), file, "");
-                Console.WriteLine("Successfully uploaded " + file);
-            }
+            var bucketName = "resalliesbucket";
 
-            catch (MinioException e)
+            foreach (var file in objFiles)
             {
-                Console.WriteLine("File Upload Error: {0}", e.Message);
+                try
+                {
+                    byte[] fileBytes;
+                    using (var ms = new MemoryStream())
+                    {
+                        file.CopyTo(ms);
+                        fileBytes = ms.ToArray();
+                    }
+
+                    using var filestream = new MemoryStream(fileBytes);
+                    PutObjectArgs poa = new PutObjectArgs()
+                    .WithBucket(bucketName)
+                    .WithObject("Images/" + file.FileName)
+                    .WithStreamData(filestream)
+                    .WithObjectSize(filestream.Length)
+                    .WithContentType("application/octet-stream");
+                    await client.PutObjectAsync(poa);
+
+                    StatObjectArgs stat = new StatObjectArgs()
+                            .WithBucket(bucketName)
+                            .WithObject("Images/" + file.FileName);
+
+                    ObjectStat objectArgs = await client.StatObjectAsync(stat);
+                }
+
+                catch (Exception ex)
+                {
+
+                }
             }
         }
     }
